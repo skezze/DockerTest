e30ecbbab8cccd8457b490f5356aceea58ea6ab1
cherrypix <genya1234732@gmail.com>
2024-02-06 20:52:32 +0200

    update to docker init

--- DockerTest.Api/Program.cs
+++ DockerTest.Api/Program.cs
@@ -8,13 +8,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
