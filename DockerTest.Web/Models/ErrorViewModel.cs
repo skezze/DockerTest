eecf8636a2344ceb625018320f28c3d1dec79679
skezze <genya1234732@gmail.com>
2024-02-23 03:21:29 +0200

    update

---
+++ DockerTest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DockerTest.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
